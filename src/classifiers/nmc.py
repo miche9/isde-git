97bbda4e19620da2916d916260a5e29b1e028c6e
michele <michelemereu94@gmail.com>
2023-10-23 18:29:19 +0200

    Added function predict

--- src/classifiers/nmc.py
+++ src/classifiers/nmc.py
@@ -38,4 +38,22 @@ class NMC(object):
         pass
 
     def predict(self, xts):
-        pass
+        """
+                Compute predictions on test data.
+
+                Parameters
+                ----------
+                xts
+                    Test data
+
+                Returns
+                -------
+                Predicted labels for the test data.
+
+                """
+        if self._centroids is None:
+            raise ValueError("Centroids not set. Run fit(x,y) first!")
+
+        dist = pairwise_distances(xts, self._centroids)
+        ypred = np.argmin(dist, axis=1)
+        return ypred
